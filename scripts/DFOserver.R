0f1f2f07ea27cf64868a38150f0cdad12a86b81b
LEEYUHC <leah.crowe@noaa.gov>
2019-06-13 23:31:30 -0300

    add progress bar

--- scripts/DFOserver.R
+++ scripts/DFOserver.R
@@ -5,21 +5,25 @@ output$finalmess <- renderText({
   # 'size', 'type', and 'datapath' columns. The 'datapath'
   # column will contain the local filenames where the data can
   # be found.
+  disable("mappdf")
   inFile <- input$egcanada
   
   if (is.null(inFile)){
     return("")
     print("no pathway")
   }else{
-    
+      
     egdaily<-read.csv(inFile$datapath, header = TRUE, stringsAsFactors = FALSE)
     print(egdaily)
-    output$finalmess<-renderText({"Great job"})
+    output$finalmess<-renderText({""})
+    
+    withProgress(message = 'Analyzing sightings for new protection zones...', value = 0, {
     shapepath<-"./shapefiles"
     
     source('./scripts/shapefiles.R', local = TRUE)$value
+    incProgress(1/5)
     source('./scripts/trigger analysis.R', local = TRUE)$value
-    
+    })
   }
   
 })
