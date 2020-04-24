c80b1b05fc24da90d78ab3444e8288a8019c26a2
leahcrowe <leah.crowe@noaa.gov>
2020-04-24 18:45:54 -0400

    new shapefiles, formatting

--- scripts/shapefiles.R
+++ scripts/shapefiles.R
@@ -12,12 +12,13 @@
 ######
 ##import shapefiles
   
+  ##Full Atlantic grid that we got from DFO via K. Davies Oct 2019
+  ## read and transform this differently to crop it later (I have only been successful in package sf)
+  ATL_grid<-st_read(shapepath, layer = "Full_ATL_grids")
+  
   ##management measures
   ##dynamic fishing grid
   dynafish<-readOGR(shapepath, layer = "2020_Dynamic_zone")
-  ##Full Atlantic grid that we got from DFO via K. Davies Oct 2019
-  ATL_grid<-readOGR(shapepath, layer = "Full_ATL_grids")
-  
   ##dynamic shipping
   dynaship<-readOGR(shapepath, layer = "shiplane")
   ##shipping slow zone July2019
@@ -25,28 +26,32 @@
 
   ##other
   ##st. pierre et micquelon
-spm<-readOGR(shapepath, layer = "spm")
+  spm<-readOGR(shapepath, layer = "spm")
   ##critical habitat
-crit_habi<-readOGR(shapepath, layer = "NARW_Critical_Habitat")
+  crit_habi<-readOGR(shapepath, layer = "NARW_Critical_Habitat")
   ##10 & 20 fathom lines
-fath_10<-readOGR(shapepath, layer = "fath_10")
-fath_20<-readOGR(shapepath, layer = "fath_20")
+  fath_10<-readOGR(shapepath, layer = "fath_10")
+  fath_20<-readOGR(shapepath, layer = "fath_20")
 
 ## projected properly
+ATL_grid.tr<-st_transform(ATL_grid, CRS.new)
+
 dynafish.tr<-spTransform(dynafish, CRS.new)
 dynaship.tr<-spTransform(dynaship, CRS.new)
 shipzone.tr<-spTransform(shipzone, CRS.new)
-ATL_grid.tr<-spTransform(ATL_grid, CRS.new)
+
 spm.tr<-spTransform(spm, CRS.new)
 crit_habi.tr<-spTransform(crit_habi, CRS.new)
 fath_10.tr<-spTransform(fath_10, CRS.new)
 fath_20.tr<-spTransform(fath_20, CRS.new)
 
 ######  
+ATL_grid.sp<-st_transform(ATL_grid.tr,CRS.latlon)
+
 dynafish.sp<-spTransform(dynafish.tr,CRS.latlon)
 dynaship.sp<-spTransform(dynaship.tr,CRS.latlon)
 shipzone.sp<-spTransform(shipzone.tr,CRS.latlon)
-ATL_grid.sp<-spTransform(ATL_grid.tr,CRS.latlon)
+
 spm.sp<-spTransform(spm.tr,CRS.latlon)
 crit_habi.sp<-spTransform(crit_habi.tr, CRS.latlon)
 fath_10.sp<-spTransform(fath_10.tr, CRS.latlon)
